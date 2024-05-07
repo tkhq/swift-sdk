1f413f5ed24e6f9cb175e9d10a549bede8ea475f
Taylor Dawson <taylor@turnkey.io>
2024-05-07 11:07:09 +0300

    Move extensions to separate file

---
+++ Sources/Shared/P256+String.swift
@@ -0,0 +1,78 @@
+//
+//  File.swift
+//  
+//
+//  Created by Taylor Dawson on 5/7/24.
+//
+
+import CryptoKit
+import Foundation
+
+enum KeyError: Error {
+  case representationUnavailable(String)
+}
+
+public protocol PublicKeyProtocol {
+  var rawRepresentation: Data { get }
+  var x963Representation: Data { get }
+  
+  @available(iOS 16.0, macOS 13.0, watchOS 9.0, tvOS 16.0, *)
+  var compressedRepresentation: Data { get }
+  
+  var compactRepresentation: Data? { get }
+}
+
+extension P256.Signing.PublicKey: PublicKeyProtocol {}
+extension P256.KeyAgreement.PublicKey: PublicKeyProtocol {}
+
+public enum PublicKeyRepresentation {
+  case raw
+  case x963
+  case compressed
+  case compact
+}
+
+extension PublicKeyProtocol {
+  /// Converts the public key into the specified string representation.
+  ///
+  /// - Parameter representation: The desired representation of the key.
+  /// - Returns: A string representation of the public key in the specified format.
+  /// - Throws: `KeyError.representationUnavailable` if the desired representation is not available.
+  public func toString(representation: PublicKeyRepresentation) throws -> String {
+    switch representation {
+    case .raw:
+      return rawRepresentation.toHexString()
+    case .x963:
+      return x963Representation.toHexString()
+    case .compressed:
+      if #available(iOS 16.0, macOS 13.0, watchOS 9.0, tvOS 16.0, *) {
+        return compressedRepresentation.toHexString()
+      } else {
+        throw KeyError.representationUnavailable("Compressed representation is unavailable.")
+      }
+    case .compact:
+      return compactRepresentation?.toHexString() ?? ""
+    }
+  }
+}
+
+public enum PrivateKeyRepresentation {
+  case raw
+  case x963
+}
+
+extension P256.Signing.PrivateKey {
+  /// Converts the private key into the specified string representation.
+  ///
+  /// - Parameter representation: The desired representation of the key.
+  /// - Returns: A string representation of the private key in the specified format.
+  /// - Throws: `KeyError.representationUnavailable` if the desired representation is not available.
+  public func toString(representation: PrivateKeyRepresentation) throws -> String {
+    switch representation {
+    case .raw:
+      return rawRepresentation.toHexString()
+    case .x963:
+      return x963Representation.toHexString()
+    }
+  }
+}
