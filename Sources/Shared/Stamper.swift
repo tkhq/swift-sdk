6667926aef136854cc61bb25c40963500ccb849c
Taylor Dawson <taylor@turnkey.io>
2024-11-28 03:53:14 -0800

    Formatting

--- Sources/Shared/Stamper.swift
+++ Sources/Shared/Stamper.swift
@@ -117,11 +117,14 @@ public class Stamper {
         }
       }
 
-        guard let challenge = payload.compactMap({ String(format: "%02x", $0) }).joined().data(using: .utf8) else {
+      guard
+        let challenge = payload.compactMap({ String(format: "%02x", $0) }).joined().data(
+          using: .utf8)
+      else {
         continuation.resume(throwing: StampError.assertionFailed)
-            return
+        return
       }
-        
+
       self.passkeyManager?.assertPasskey(challenge: challenge)
     }
   }
